7ae6f999336b9672cf30f4e0bdf41e515af30061
Benjamin Toomer <mullet1989@gmail.com>
2018-02-27 19:27:32 +0000

    changing the project to use .less style sheets now

--- src/app/manager-dashboard/manager-dashboard.component.ts
+++ src/app/manager-dashboard/manager-dashboard.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-manager-dashboard',
   templateUrl: './manager-dashboard.component.html',
-  styleUrls: ['./manager-dashboard.component.css']
+  styleUrls: ['./manager-dashboard.component.less']
 })
 export class ManagerDashboardComponent implements OnInit {
 
