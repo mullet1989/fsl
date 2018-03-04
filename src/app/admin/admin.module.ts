787fa69b8619c39225001bafad8ceb7ad6ab39e1
Benjamin Toomer <mullet1989@gmail.com>
2018-03-04 22:13:10 +0000

    missing import on production build

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -5,12 +5,14 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { AdminRoutingModule } from './admin.routing-module';
 import { MaterialDesignModule } from '../material-design.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
-    MaterialDesignModule
+    MaterialDesignModule,
+    RouterModule
   ],
   declarations: [
     AdminComponent,
